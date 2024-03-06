770125d402ec2cd111811ee688f7342d61736f78
Yasmin Teles <teles.yasmin.tech@gmail.com>
2024-03-06 14:08:46 -0300

    feat: add ssh key script

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: help del
+.PHONY: help del ssh
 
 help: ## Show help.
 	@printf "A set of development's commands.\n"
@@ -8,5 +8,10 @@ help: ## Show help.
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\t\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
 del: ## delete your environment configuration
-	@stow --dotfiles -D git
+	@stow --dotfiles -D git ssh
 	@rm -rf ~/Projects
+	@rm -rf ~/.ssh/id_ed25519 ~/.ssh/id_ed25519.pub
+
+ssh: ## Copy the SSH public key to your clipboard.
+	@pbcopy < ~/.ssh/id_ed25519.pub
+	@echo "Add the SSH public key to your account on GitHub or GiLlab. See your profile settings for more information."
--- install.sh
+++ install.sh
@@ -2,18 +2,25 @@
 
 read -p "What is your email? " GIT_EMAIL
 
-print "\nPlease wait! It will configure your environment. \n"
-echo "[1/3] Creating the Projects folder..."
+printf "\nPlease wait! It will configure your environment.\n"
+echo "[1/4] Creating the Projects folder..."
 mkdir ~/Projects
 
-echo "[2/3] Setting up dotfiles..."
+echo "[2/4] Setting up dotfiles..."
 git clone https://github.com/YasminTeles/dotfiles.git ~/.dotfiles
 
 cd ~/.dotfiles
-stow --dotfiles git
+stow --dotfiles git ssh
 
-echo "[3/3] Configuring Git..."
+echo "[3/4] Configuring Git..."
 git config --global user.email $GIT_EMAIL
 
-printf "\nSuccess Everythings is ready!"
-printf "\nHave a nice day!"
+echo "[4/4] Generating a new SSH key..."
+rm -rf ~/.ssh/id_ed25519 ~/.ssh/id_ed25519.pub
+ssh-keygen -t ed25519 -C $GIT_EMAIL -f ~/.ssh/id_ed25519 -q -N ""
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+echo "Warning: You must add the SSH public key to your account on GitHub or GitLab. For more information, see make ssh"
+
+printf "\nSuccess: Everything is ready!"
+printf "\nHave a nice day!\n"
