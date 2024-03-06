6e822336a21a736764e337c96a3a8bddac428a4e
Yasmin Teles <teles.yasmin.tech@gmail.com>
2024-03-06 11:25:55 -0300

    feat: add git config at install script

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-echo "[1/2] Creating the Projects folder..."
+read -p "What is your email? " GIT_EMAIL
+
+print "\nPlease wait! It will configure your environment. \n"
+echo "[1/3] Creating the Projects folder..."
 mkdir ~/Projects
 
-echo "[2/2] Setting up dotfiles..."
+echo "[2/3] Setting up dotfiles..."
 git clone https://github.com/YasminTeles/dotfiles.git ~/.dotfiles
 
 cd ~/.dotfiles
 stow --dotfiles git
 
-echo "Success Everythings is ready!"
-echo "Have a nice day!"
+echo "[3/3] Configuring Git..."
+git config --global user.email $GIT_EMAIL
+
+printf "\nSuccess Everythings is ready!"
+printf "\nHave a nice day!"
