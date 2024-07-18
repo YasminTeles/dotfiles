462288ae8bca7f158e25136c56334b2888f6aea1
Yasmin Teles <yasmin.gomes@olxbr.com>
2024-07-18 10:43:51 -0300

    feat: add screenshots configs for macos

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -2,5 +2,15 @@
 
 echo "Setting up macos..."
 
-# Disable the system UI sound effects
+###############################################################################
+# Screenshots                                                                 #
+###############################################################################
+
+# Disable the system UI sound effects.
 defaults write com.apple.systemsound "com.apple.sound.uiaudio.enabled" -int 0
+
+# Remove the default shadow from screenshots.
+defaults write com.apple.screencapture "disable-shadow" -bool "true"
+
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
