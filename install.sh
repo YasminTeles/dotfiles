52d4262745e7d401818057c13060d463358470f2
Yasmin Teles <teles.yasmin.tech@gmail.com>
2025-06-09 17:58:02 -0300

    feat: add sudo session support

--- install.sh
+++ install.sh
@@ -3,6 +3,16 @@
 # Tells the shell script to exit if it encounters an error
 set -e
 
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Print a step description
 TOTAL_STEPS=13
 STEP=1
