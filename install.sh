6bc023c44b3c1fd1b411beadf944964cd55f56c4
Yasmin Teles <teles.yasmin.tech@gmail.com>
2025-06-10 10:46:08 -0300

    fix: remove close system preferences panes

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 
 # Close any open System Preferences panes, to prevent them from overriding
 # settings we’re about to change
-osascript -e 'tell application "System Preferences" to quit'
+# osascript -e 'tell application "System Preferences" to quit'
 
 # Ask for the administrator password upfront
 sudo -v
