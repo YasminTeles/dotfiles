6c1b1590b88dfa1bd620fcf1faab0c951db756e0
Yasmin Teles <yasmin.teles@vtex.com>
2025-08-27 11:33:56 -0300

    fix: improve login script with Gum

--- scripts/login.sh
+++ scripts/login.sh
@@ -4,16 +4,16 @@ function step_msg {
 	printf "\033[35;1m%s...\033[0m\n" "$1";
 }
 
-# AWS login and docker login
+# AWS and Docker Images Registry login
 function docker-login {
     ## Private
-    aws ecr get-login-password --region $AWS_REGION --profile $AWS_PROFILE \
+    gum spin --spinner dot -- aws ecr get-login-password --region $AWS_REGION --profile $AWS_PROFILE \
       | docker login \
           --username AWS \
           --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
     ## Public
-    aws ecr-public get-login-password --region $AWS_REGION --profile $AWS_PROFILE \
+    gum spin --spinner dot -- aws ecr-public get-login-password --region $AWS_REGION --profile $AWS_PROFILE \
       | docker login \
           --username AWS \
           --password-stdin public.ecr.aws
@@ -26,16 +26,16 @@ function login {
     source $HOME/.dotfiles/.env
     set +a
 
-    step_msg "Logging in to AWS..."
+    step_msg "Logging into the AWS"
     if [ -z "$AWS_PROFILE" ]; then
         echo "AWS_PROFILE is not set. Please set it before running this script."
         exit 1
     fi
-    aws sso login --profile $AWS_PROFILE
+    gum spin --spinner dot -- aws sso login --profile $AWS_PROFILE
 
-    step_msg "Logging in to Docker Images.."
+    step_msg "Logging into the Docker Images Registry"
     docker-login
 
-    step_msg "Logging in to Pulumi..."
-    pulumi login
+    step_msg "Logging into the Pulumi"
+    gum spin --spinner dot --show-output -- pulumi login
 }
